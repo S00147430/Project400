035d6d16076c02dae62c8eac5503420359585224
s00143557 <undeadmonk@live.ie>
2016-06-19 14:48:06 +0100

    Lizard Man

--- wtwEnemyTesting/Assets/Enemy/Blob/BlobEnemyType2.cs
+++ wtwEnemyTesting/Assets/Enemy/Blob/BlobEnemyType2.cs
@@ -3,23 +3,35 @@ using System.Collections;
 
 public class BlobEnemyType2 : BaseEnemy
 {
-    enum BlobStates { LANDED, JUMPING }
+    /*
+        This one shall be similar to the old, however some key changes need to be put in place. It must jump directly to crash, instead of the multiple short hops to get
+        to where he is that the old one did. If I can get it to jump in such a way that halfway to the destination is when the jump reaches its apex, it will be absolutely
+        perfect. It's not a game breaking thing if I can't get it that way, but it would be nice.
 
+        Ran into several small issues, but overall it came out relatively close to what I hoped and matched what I expected. The stuff I've done here should help with the
+        lizard man, who also travels by jumping, but jumps to specific points instead of at Crash.
+    */
+
+    enum BlobStates { LANDED, JUMPING }
+    BlobStates currentState;
     GameObject target;
-    Vector3 targetPosition;
-    Vector3 originalPosition;
+    Vector3 targetPosition; // Targets position when the blob starts moving
+    Vector3 originalPosition; //(Used for keeping track of where it was before it left the ground)
     float originalXRotation;
     float originalZRotation;
     float newYRotation;
     Quaternion newRotation;
     Vector3 distanceTargetBlob;
-    Vector3 followDistance;
+    //Vector3 followDistance;
     Vector3 jumpVector;
+    bool follow;
+    bool ascending;
+    float waitTime; //Gonna try and make the blob pause for a second before jumping again.
     
-    float jumpHeight;
-
+    public float jumpHeight;
     public float FollowDistance;
     public float SpeedMultiplier;
+    float groundDistance;
 
     void Start ()
     {
@@ -31,17 +43,73 @@ public class BlobEnemyType2 : BaseEnemy
         newYRotation = transform.rotation.y;
         newRotation = new Quaternion(originalXRotation, newYRotation, originalZRotation, 1.0f);
         transform.rotation = newRotation;
-        followDistance = new Vector3(FollowDistance, 0, FollowDistance);
+        //followDistance = new Vector3(FollowDistance, 0, FollowDistance);
+        jumpVector = new Vector3(0, jumpHeight);
+        currentState = BlobStates.LANDED;
+        follow = false;
+        groundDistance = GetComponent<Collider>().bounds.extents.y;
+        waitTime = 1.0f;
     }
 	
 	void Update ()
     {
-        distanceTargetBlob = target.transform.position - transform.position;
-
-        if(distanceTargetBlob < followDistance)
+        if (Vector3.Distance(transform.position, target.transform.position) > FollowDistance)
         {
-
+            //Debug.Log("Out of Range");
+            follow = false;
         }
-	    
-	}
+
+        if (Vector3.Distance(transform.position, target.transform.position) <= FollowDistance)
+        {
+            //Debug.Log("Within Range");
+            follow = true;
+            
+        }
+
+        if (follow == true)
+        {
+            if(IsGrounded())
+            {
+                //Debug.Log("Is on the ground");
+                originalPosition = transform.position;
+                ascending = true;
+                currentState = BlobStates.LANDED;
+                targetPosition = target.transform.position;
+                distanceTargetBlob = targetPosition - transform.position;
+            }
+
+            if(transform.position.y >= jumpHeight + originalPosition.y)
+            {
+                ascending = false;
+                //Debug.Log("Apex of Jump");
+            }
+
+            if(currentState == BlobStates.LANDED)
+            {
+                waitTime -= Time.deltaTime;
+                if(waitTime <= 0)
+                {
+                    waitTime = 1.0f;
+                    currentState = BlobStates.JUMPING;
+                    transform.position += distanceTargetBlob * Time.deltaTime * SpeedMultiplier;
+                    transform.position += jumpVector * Time.deltaTime * SpeedMultiplier;
+                }
+            }
+
+            if(currentState == BlobStates.JUMPING)
+            {
+                transform.position += distanceTargetBlob * Time.deltaTime * SpeedMultiplier;
+                if(ascending == true)
+                    transform.position += jumpVector * Time.deltaTime * SpeedMultiplier;
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {//found on the unity forums, may fix the problem of detecting when the blob hits the ground
+        #region url of unityforums answer
+            http://answers.unity3d.com/questions/196381/how-do-i-check-if-my-rigidbody-player-is-grounded.html
+        #endregion
+        return Physics.Raycast(transform.position, -Vector3.up, groundDistance + 0.1f);
+    }
 }
---
+++ wtwEnemyTesting/Assets/Enemy/Lizardman/LizardmanEnemy.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LizardmanEnemy : BaseEnemy
+{
+    /*
+        Translating a point in Unity doesn't do what I expect it to. Which is translate a point towards a given point. Local and Global transforms,
+        and the differences between them, are likely the problem. Looks like we're back to good ol' Vector addition for now.
+
+        Dissapears on start, no idea what's going on with it. Setting the point kind of helps, but then it just wanders off to the side without 'encountering'
+        the other placements. (The line of code marked '// To be sure.' sets the point to the current stop point.
+
+        I wanted translations to work because then we wouldn't have this problem of things continuing to move once they've reached where I want. that's the problem with
+        vectors, they just keep going. I was hoping to avoid setting up colliders, but that might be the way to go now. Irritating.
+
+        Got the translations going properly. Logic error on my end, didn't parse what was actually going on in a translate. Anyway, new issue.
+        Gets stuck on the next point and keeps jumping forever. Don't. Really know what's going on.
+    */
+    public Transform[] stopPoints; //set size and add transforms in the inspector. Make sure the transforms are in the correct order
+    public int currentStopPoint;
+    int arrayLength;
+    enum Direction { FORWARD, BACK } //whether the index is increasing or decreasing
+    enum LizardState { LANDED, JUMPED, LANDING }
+    Direction currentDirection;
+    LizardState currentState;
+    Transform target;
+    Vector3 targetVector;
+    float distance;
+    public float upSpeed;
+    bool initial; //this is to show that the object has just started so it can be set to false such that the fecking thing will stop being teleported to the next point, which is also the destination
+
+
+	void Start ()
+    {
+        initial = true;
+        currentDirection = Direction.FORWARD;
+        currentState = LizardState.LANDED;
+	}
+	
+	void Update ()
+    {
+        if (currentDirection == Direction.FORWARD)
+        {
+            if (currentState == LizardState.LANDED)
+            {
+                if (currentStopPoint != arrayLength - 2)
+                {
+                    if(initial != true)
+                    {
+                        currentStopPoint += 1;
+                    }
+                    initial = false;
+                    transform.position = stopPoints[currentStopPoint].position; // just to be sure
+                    target = stopPoints[currentStopPoint + 1];
+                    distance = Vector3.Distance(transform.position, target.position);
+                    targetVector = target.position - transform.position;
+                    //transform.position += targetVector;
+                    transform.Translate(targetVector * Time.deltaTime);
+                    currentState = LizardState.JUMPED;
+                }
+                else if (currentStopPoint == arrayLength - 2)
+                {
+                    currentStopPoint += 1;
+                    currentDirection = Direction.BACK;
+                }
+            }
+        }
+
+        if (currentDirection == Direction.BACK)
+        {
+            if (currentState == LizardState.LANDED)
+            {
+                if (currentStopPoint != 1)
+                {
+                    currentStopPoint -= 1;
+                    transform.position = stopPoints[currentStopPoint].position; // just to be sure
+                    target = stopPoints[currentStopPoint - 1];
+                    distance = Vector3.Distance(transform.position, target.position);
+                    targetVector = transform.position - target.position;
+                    //transform.position += targetVector;
+                    transform.Translate(targetVector * Time.deltaTime);
+                    currentState = LizardState.JUMPED;
+                }
+                else if (currentStopPoint == 1)
+                {
+                    currentStopPoint -= 1;
+                    currentDirection = Direction.FORWARD;
+                }
+            }
+        }
+        if (currentState == LizardState.JUMPED)
+        {
+            //if(Vector3.Distance(transform.position, target.position) > distance / 2) //I forgot that distance would be affected by vertical positioning as well. Jumping was throwing it off
+            if (transform.position.x - target.position.x > distance / 2)
+            {
+                transform.Translate(targetVector * Time.deltaTime);
+                transform.position += Vector3.up * Time.deltaTime * upSpeed;
+            }
+            else
+            {
+                currentState = LizardState.LANDING;
+            }
+        }
+
+        if(currentState == LizardState.LANDING)
+        {
+            transform.Translate(targetVector * Time.deltaTime);
+            //transform.position -= Vector3.up * Time.deltaTime * upSpeed;
+            if (transform.position == target.position)
+            {
+                currentState = LizardState.LANDED;
+            }
+        }
+	}
+}
